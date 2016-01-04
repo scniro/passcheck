602f7312b2875320951c16f11d1743988dde7f2f
Salvatore Niro <salvatore.niro@outlook.com>
2016-01-04 16:17:37 -0700

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
 var concat = require('gulp-concat');
+var fs = require('fs');
 
 gulp.task('test', function(){
     return gulp.src('passcheck.test.js')
@@ -16,4 +17,19 @@ gulp.task('concat-ng', function(){
     return gulp.src(['C:/Development/GitHub/ngPasscheck/ngPasscheck/ng-passcheck/src/ng.js', 'passcheck.js'])
         .pipe(concat('ng-passcheck.js', {newLine: '\r\n'}))
         .pipe(gulp.dest('C:/Development/GitHub/ngPasscheck/ngPasscheck/ng-passcheck/src'));
+});
+
+gulp.task('passwords', function() {
+    function isNotEmpty(line) {
+        return Boolean(line.length);
+    }
+
+    function parseLine(line) {
+        return line.trim();
+    }
+
+    var lines = fs.readFileSync('passwords.txt').toString().split('\n');
+    var result = lines.filter(isNotEmpty).map(parseLine);
+    var json = JSON.stringify({ 'dictionary': result });
+    fs.writeFileSync('passwords.json', json);
 });
--- passcheck.js
+++ passcheck.js
@@ -67,7 +67,7 @@ function passcheck() {
 
     function eval(value) {
 
-        var result = { 'weak': false, 'medium': false, 'strong': false, 'score': 0 };
+        var result = {'weak': false, 'medium': false, 'strong': false, 'score': 0};
         var tier = 0;
 
         if (self.configuration.common.test) {
@@ -97,9 +97,9 @@ function passcheck() {
     function score(value, tier) {
 
         var special = {
-            'specialCharacters': { 'count': /[^\w\s]/.test(value) ? value.match(/[^\w\s]/g).length : 0 },
-            'capitalCharacters': { 'count': /[A-Z]/.test(value) ? value.match(/[A-Z]/g).length : 0 },
-            'numericCharacters': { 'count': /\d+/.test(value) ? value.match(/\d/g).length : 0 }
+            'specialCharacters': {'count': /[^\w\s]/.test(value) ? value.match(/[^\w\s]/g).length : 0},
+            'capitalCharacters': {'count': /[A-Z]/.test(value) ? value.match(/[A-Z]/g).length : 0},
+            'numericCharacters': {'count': /\d+/.test(value) ? value.match(/\d/g).length : 0}
         }
 
         var bonus = 0;
@@ -155,14 +155,11 @@ function passcheck() {
 
                 var limit = self.configuration.common && self.configuration.common.limit || 10000;
                 var fs = require('fs');
-                var arr = limit !== 10000 ?
-                    fs.readFileSync('./passwords.txt').toString().split('\n').slice(0, limit) :
-                    fs.readFileSync('./passwords.txt').toString().split('\n');
+                var dictionary = require('./passwords.json').dictionary
 
-                for (var i in arr)
-                    common.push(arr[i].trim());
+                common = limit !== 10000 ? dictionary.slice(0, limit) : dictionary
             }
-        } else {
+        } else if (typeof angular !== 'undefined') {
 
             var $http = angular.injector(['ng']).get('$http');
             var $q = angular.injector(['ng']).get('$q');
@@ -180,6 +177,8 @@ function passcheck() {
             });
 
             return deferred.promise;
+        } else {
+            self.configuration.common.test = false;
         }
     }
 
